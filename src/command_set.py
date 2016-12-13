0c5e54743fdb525f4a7c424d1a4cf91f79ec7be1
aaronlbrink <aaron@brinkdatabase.com>
2016-12-13 14:21:10 -0500

    Finalized unit testing

--- src/command_set.py
+++ src/command_set.py
@@ -8,6 +8,7 @@ class CommandSet:
         # also this goes through the list in order so for example 'timer set' should be put before 'timer'
         for command in self._commands:
             # while looking through all the commands in the list, see if the spoken text matches a command in the list
+            # uses the matches method from the command class which uses regex to match usually the first part of the spoken string
             if command.matches(spoken_text):
                 return command.run(spoken_text)
         
---
+++ src/command_set_test.py
@@ -0,0 +1,53 @@
+import command
+import command_set
+import unittest
+
+class MockCommand(command.Command):
+    def __init__(self, regex):
+        command.Command.__init__(self, regex)
+        self.run_called = False
+
+        
+    def run(self, spoken_text):
+        self.run_called = True
+
+class CommandSetTests(unittest.TestCase):
+    def test_not_match(self):
+        ''' Tests that the regex matching functionality works (by testing things that don't match) '''
+        mock_command_a = MockCommand(r'^hey')
+        mock_command_b = MockCommand(r'^there')
+        my_command_set = command_set.CommandSet([
+            mock_command_a,
+            mock_command_b,
+            ])
+        my_command_set.handle_command('blah')
+        assert not mock_command_a.run_called, 'non-matching command A was run when it should\'nt\'ve been'
+        assert not mock_command_b.run_called, 'non-matching command B was run incorrectly'
+
+    def test_match(self):
+        ''' Tests that the regex matching functionality works '''
+        mock_command_a = MockCommand(r'^hey')
+        mock_command_b = MockCommand(r'^there')
+        my_command_set = command_set.CommandSet([
+            mock_command_a,
+            mock_command_b,
+            ])
+        my_command_set.handle_command('hey')
+        assert mock_command_a.run_called, 'non-matching command A was run and it should have'
+        assert not mock_command_b.run_called, 'non-matching command B was run incorrectly'
+
+    def test_first_match(self):
+        ''' Tests that the regex matching functionality chooses the first option in the list'''
+        mock_command_a = MockCommand(r'^hey')
+        mock_command_b = MockCommand(r'^hey')
+        mock_command_c = MockCommand(r'^hey')
+        my_command_set = command_set.CommandSet([
+            mock_command_a,
+            mock_command_b,
+            mock_command_c,
+            ])
+        my_command_set.handle_command('hey')
+        assert mock_command_a.run_called, 'make sure command_a runs (because it\'s the first in the list'
+        assert not mock_command_b.run_called, 'make sure command B doesn\'t run'
+        assert not mock_command_c.run_called, 'make sure that command C does not run'
+
